0f2b87f8ac8ee524d167e924fee271aeb90b0f2f
inoulen inoulen <inoulenkon@gmail.com>
2023-12-26 22:34:12 +0530

    Update the likes controller actions

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,10 +1,17 @@
 class LikesController < ApplicationController
   def create
+    @like = current_user.likes.new(like_params)
+    if !@like.save
+      flash[:alert] = "Yor already liked this"
+    end
   end
 
   def destroy
+    @like = current_user.likes.find(params[:id])
+    @like.destroy
   end
 
   def like_params
+    params.require(:like).permit(:post_id)
   end
 end
