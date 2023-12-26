03547f021eee7978900e86fe28ee3c48c807c2fe
inoulen inoulen <inoulenkon@gmail.com>
2023-12-26 11:20:45 +0530

    Add user_id to the params for posts controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -70,6 +70,6 @@ class PostsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def post_params
-      params.require(:post).permit(:title, :description, :image, :video)
+      params.require(:post).permit(:title, :description, :image, :video, :user_id)
     end
 end
